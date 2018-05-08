bab88655c73759d289a77c4dfd4ca526ee81722f
Corbett Martin <corbtastik@gmail.com>
2018-05-08 10:25:14 -0500

    added sleuth

--- src/main/java/io/todos/TodosAPI.java
+++ src/main/java/io/todos/TodosAPI.java
@@ -3,6 +3,7 @@ package io.todos;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
